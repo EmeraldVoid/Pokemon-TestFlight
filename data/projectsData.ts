73d18a8e9a801337c266b5b75edc0bb3a795e85e
EmeraldVoid <69017131+EmeraldVoid@users.noreply.github.com>
2025-06-16 00:22:12 -0500

    Update projectsData.ts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,7 +11,7 @@ const projectsData: Project[] = [
     description: `A fan-made Pokémon game focused on open-world exploration and experimentation, built with Pokémon Essentials and PSDK.
     The game is currently in development but does not have a release date yet.`,
     imgSrc: '/static/images/google.png',
-    href: 'https://github.com/EmeraldVoid/Pokemon-TestFlight',
+    href: 'https://github.com/EmeraldVoid/Pokemon-TestFlight/releases',
   },
   
 ]
